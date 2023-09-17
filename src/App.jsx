f5d45026e38d6c78077dc4620b5895876bae085e
Husniddin <122876021+iamhusniddin@users.noreply.github.com>
2023-09-17 16:59:06 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,72 @@
 import { useState } from "react";
 import "./App.css";
 import Card from "./Card";
+import {Button, message} from 'antd'
+import { useEffect } from "react";
 
 function App() {
   const [inputVal, setInputVal] = useState("");
-  const [selectVal, setSelectVal] = useState("");
-  const [list, setList] = useState([])
-  
+  const [selectVal, setSelectVal] = useState("high");
+  const [list, setList] = useState([]);
+
+  const checkItem = (ids) => {
+    let newArr = list.map((item) => {
+      return item.id === ids ? { ...item, isDone: !item.isDone } : item;
+    });
+    setList(newArr);
+  };
 
   
-  console.log(list);
-  
+
+  // 1 holat
+  // useEffect(()=>{
+  //   console.log('render');
+  // }); ---- har component render bo'lganda ishlaydi
+
+  // 2 holat
+  // useEffect(()=>{
+  //   console.log('render');
+  // },[]) ---- page birinchi refresh bo'lganda
+
+  // 3 holat
+  // useEffect(()=>{
+  //   console.log('render');
+  // }, selectVal) ---- birinchi statega qaram bo'lish
+
+  // 4 holat
+  // useEffect(()=>{
+  //   console.log('render');
+  // },[selectVal, inputVal]) ---- ko'pgina statega qaram bo'lish
+
+
+  // useEffect(()=>{
+  //   fetch ('https://jsonplaceholder.typicode.com/users')
+  //   .then(res=>res.json())
+  //   .then(data=>console.log(data))
+  //   .catch(error=>alert('xato'))
+  //   .finally(()=>console.log('tugadi'))
+  // },[])
+
+
+  // useEffect(()=>{
+  //   async function fetcher() {
+  //     try {
+  //       const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //       const data = await res.json()
+        
+  //     } catch (error) {
+  //       alert(error)
+  //     }
+  //   }
+  // },[])
+
+
+
+
+
+  // const additem = () =>{
+  //   setList([...list, {title: input}])
+  // }
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
@@ -19,29 +75,59 @@ function App() {
           Shopping List App
         </h1>
 
-        <div className="flex gap-3">
+        <div className="flex gap-3 items-center">
           <input
-          value={inputVal}
-            onChange={(e) =>setInputVal(e.target.value)}
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
             className="rounded-lg p-3"
             type="text"
             placeholder="item title"
             name=""
             id=""
           />
-          <select value={selectVal} onChange={(e)=> setSelectVal(e.target.value)} className="rounded-lg p-3" name="" id="">
+          <select
+            value={selectVal}
+            onChange={(e) => setSelectVal(e.target.value)}
+            className="rounded-lg p-3"
+            name=""
+            id=""
+          >
             <option value="high">High</option>
-            <option value="low">Low</option>
             <option value="normal">Normal</option>
+            <option value="low">Low</option>
           </select>
-          <button onClick={()=> setList([...list, {title: inputVal, priority: selectVal}])} className=" rounded-lg bg-indigo-500 p-3 text-white">
+          <Button
+            onClick={() =>{
+             if (inputVal.length > 0 && selectVal){
+                 setList([
+                    ...list,
+                    {
+                      id: Date.now(),
+                      title: inputVal,
+                      priority: selectVal,
+                      isDone: false,
+                    },
+                  ]);
+                  message.success("Item is added");
+                  setInputVal("")
+             }
+             else message.warning("Fill the input!")
+            }}
+            className=" rounded-lg bg-indigo-500  text-white "
+          >
             Add
-          </button>
+          </Button>
         </div>
         <div className="mt-5 w-full flex-col gap-[10px]">
-        {list.map((item)=>(
-          <Card title={item.title} pri={item.priority}/>
-        ))}
+          {list.map((item) => (
+            <Card
+              id={item.id}
+              title={item.title}
+              pri={item.priority}
+              checkItem={checkItem}
+              isDone={item.isDone}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
-export default function Card({title, pri}) {
-    
+
+export default function Card({ id, title, pri, checkItem, isDone }) {
+
+
+  const style = () =>{
+    if(pri === "high") return 'red';
+    else if (pri === "normal") return "yellow";
+    else if (pri === "low") return "green"
+  };
+
+  
 
 
   return (
-    <div className='flex justify-between bg-white shadow-lg rounded-lg items-center p-3 '>
-        <p className='text-[18px]'>{title}</p>
-        <p className='text-[15px] text-gray-600'>{pri}</p>
+    <div style={{ backgroundColor: style(), filter:isDone && "grayscale(100%)" }} className='flex justify-between mt-3 bg-white shadow-lg rounded-lg items-center p-3 '>
+        <p className='text-[18px]' style={{textDecoration: isDone && "line-through"}}>{title}</p>
+        {/* <p className='text-[15px] text-gray-600'>{pri}</p> */}
+        <input type="checkbox" name="" id="" onChange={()=> checkItem(id)} />
+
     </div>
   )
 }
